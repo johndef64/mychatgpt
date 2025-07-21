8c948189e0c07d92540916420f9c14540f95a6c4
johndef64 <116732521+johndef64@users.noreply.github.com>
2025-07-21 12:38:06 +0200

    remove chatbot into dedicated repo

--- mychatbot/mychatbot.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM powershell -Command "Start-Process 'streamlit' -ArgumentList 'run MyChatbot.py --server.headless true' -WindowStyle Hidden"
-streamlit run MyChatbot.py --server.headless true
