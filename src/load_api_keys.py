93d91ff72f409e74a885fa7f5e17d4203b2b0eba
johndef64 <116732521+johndef64@users.noreply.github.com>
2026-01-29 16:05:05 +0100

    Update load_api_keys.py

--- src/load_api_keys.py
+++ src/load_api_keys.py
@@ -14,3 +14,9 @@ api_keys.keys()
 for key in api_keys:
     if key is string:
         os.environ[key.upper() + "_API_KEY"] = api_keys[key]
+        
+# print all os environment variables that end with _API_KEY
+for key, value in os.environ.items():
+    if key.endswith("_API_KEY"):
+        # print(f"{key}: {value[:4]}...{value[-4:]}")
+        pass
